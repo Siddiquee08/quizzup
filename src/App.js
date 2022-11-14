f0c83184eeb6b37d346ec2c47038e60ca11d45f8
T.M Siddiquee <siddiquee09@gmail.com>
2022-11-15 01:25:28 +0600

    build

--- src/App.js
+++ src/App.js
@@ -1,43 +1,49 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './App.css';
-import Home from '../../cs-quiz/src/components/Home/Home'
-import Main from './layout/Main';
-import Statistics from './components/Statistics/Statistics';
-import Blogs from './components/Blogs/Blogs';
-import Exampage from './components/Exampage/Exampage';
-import { ToastContainer } from 'react-toastify';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Home from "../../quizzup/src/components/Home/Home";
+import Main from "./layout/Main";
+import Statistics from "./components/Statistics/Statistics";
+import Blogs from "./components/Blogs/Blogs";
+import Exampage from "./components/Exampage/Exampage";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   const router = createBrowserRouter([
     {
-      path: '/', element: <Main></Main>, children: [
-        
+      path: "/",
+      element: <Main></Main>,
+      children: [
         {
-          path: '/',
-          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
-          element: <Home></Home>
+          path: "/",
+          loader: () => fetch("https://openapi.programming-hero.com/api/quiz"),
+          element: <Home></Home>,
         },
         {
-          path: 'statistics',
+          path: "statistics",
           loader: () => fetch(`https://openapi.programming-hero.com/api/quiz`),
-          element: <Statistics></Statistics>
+          element: <Statistics></Statistics>,
         },
         {
-          path: 'exams/:id',
-          loader: ({params})=>{
-            return fetch(`https://openapi.programming-hero.com/api/quiz/${params.id}`)
-          } ,
-          element: <Exampage></Exampage>
+          path: "exams/:id",
+          loader: ({ params }) => {
+            return fetch(
+              `https://openapi.programming-hero.com/api/quiz/${params.id}`
+            );
+          },
+          element: <Exampage></Exampage>,
         },
         {
-          path: 'blogs', element: <Blogs></Blogs>
-        }
-      ]
+          path: "blogs",
+          element: <Blogs></Blogs>,
+        },
+      ],
     },
-    
-    
-    { path: '*', element: <div className='error'>This Page is Under Construction</div>}
-  ])
+
+    {
+      path: "*",
+      element: <div className="error">This Page is Under Construction</div>,
+    },
+  ]);
   return (
     <div className="App">
       <RouterProvider router={router}></RouterProvider>
